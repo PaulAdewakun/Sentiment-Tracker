01df5ec0998f3627c8b95277f86b63fc258ca0c5
PaulAdewakun <arapauladewakun@gmail.com>
2026-08-25 21:58:16 -0600

    API Key Consealing

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
 #Importing alpha vantage into program after having installed it in the terminal using python -m pip install alpha_vantage
 from alpha_vantage.timeseries import TimeSeries
+import time
+import os
+
+API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")  # Load your API key from an environment variable for security
 
 #Creating a timeseries object, api key loaded in below
-ts = TimeSeries(key='N151MRIZDCYHBXWQ', output_format='json')
+ts = TimeSeries(key=API_KEY, output_format='json')
 
-#  historical_price = historical price data, ts.get_daily returns a tuple with the first element being the data and the second being metadata
+
+
+
+
+# historical_data = historical price data, ts.get_daily_adjusted returns a tuple with the first element being the data and the second being metadata
 # meta_data = metadata about the data, such as the last refreshed date and the interval of the data
-historicalce, meta_data = ts.get_daily(symbol='AAPL')
+historical_data, meta_data = ts.get_daily_adjusted(symbol='AAPL')
